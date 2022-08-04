e464b34c864441e626bc9b929983d13b8201b514
Alexander Tarasov <tarassov.al@gmail.com>
2022-08-04 18:52:37 +0300

    style: remove quazt

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { createApp } from "vue";
 import App from "./components/App.vue";
-import { Quasar } from "quasar";
 
 import "./assets/main.css";
 
 const weatherApp = createApp(App);
-
-weatherApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
-});
 
 weatherApp.mount("#app");
