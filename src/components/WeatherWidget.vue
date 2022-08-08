1fd6a0ededd38c66109f4f5e54131a9cc75f2a7d
Alexander Tarasov <tarassov.al@gmail.com>
2022-08-09 01:59:49 +0300

    feat: widget type

---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div><MainLayout></MainLayout></div>
+</template>
+
+<style src="../assets/main.css"></style>
+<style src="primevue/resources/themes/md-light-indigo/theme.css"></style>
+<style src="primevue/resources/primevue.min.css" ;></style>
+<style src="primeicons/primeicons.css"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,48 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
+import dts from "vite-plugin-dts";
+import components from "unplugin-vue-components/vite";
+import { resolve } from "node:path";
+import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
+import { directive } from "@babel/types";
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/main.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      // Externalize deps that shouldn't be bundled into the library.
+      // external: ["vue", "@vueuse/core"],
+    },
+    sourcemap: true,
+    // Reduce bloat from legacy polyfills.
+    target: "esnext",
+    // Leave minification up to applications.
+    minify: false,
+  },
+  plugins: [
+    components({
+      dts: "src/components.d.ts",
+      resolvers: [
+        PrimeVueResolver({
+          importStyle: true,
+          importIcons: true,
+          importTheme: "md-light-indigo",
+          prefix: "Pi",
+        }),
+      ],
+      directives: true,
+      deep: true,
+    }),
+    dts(),
+    vue({
+      reactivityTransform: resolve(__dirname, "src"),
+      customElement: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
